279b0b925854eba8af12bd83113adf1e69a2e86e
buck-kristen <ste09035@byui.edu>
2015-06-05 09:03:54 -0700

    06/05/15 made it to page 36 in the pdf- work on getInput( )

--- charcoaled/src/byui/cit260/charcoaled/view/MainMenuView.java
+++ charcoaled/src/byui/cit260/charcoaled/view/MainMenuView.java
@@ -11,4 +11,35 @@ package byui.cit260.charcoaled.view;
  */
 public class MainMenuView {
     
+    private final String MENU = "\n"
+            +"\n----------------------"
+            +"|Main Menu             |"
+            +"\n----------------------"
+            +"\nN - Start New Game"
+            +"\nL - Load Existing Game"
+            +"\nH - Help"
+            +"\nS - Save Game"
+            +"\nE - Exit"
+            +"\n----------------------";
+    
+    public void displayMenu() {
+        char selection = ' ';
+        do {
+            System.out.println(MENU);//Display main menu
+            
+            String input = this.getInput(); //get user's selection
+            selection = input.charAt(0); //get first character of string
+            
+            this.doAction(selection); //do action based on selection
+        } while (selection != 'E'); //and selection is not E
+    }
+
+    private String getInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void doAction(char selection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
